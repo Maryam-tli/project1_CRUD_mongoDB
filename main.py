f7633cdaba9c5ffbf6549e2acad9c8acb79a0b0e
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-10-19 16:01:27 +0330

    I imported pymongo and made a connection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #import کتابخانه های لازم
 from tkinter import *
 from tkinter import ttk
+from pymongo import MongoClient
 
+# اتصال به MongoDB
+Client = MongoClient("localhost", 27017)
+db = Client["person"]
+persons = db["persons"]
 
 screen = Tk()
